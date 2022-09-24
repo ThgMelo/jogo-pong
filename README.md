562474000bd123f5bf5fa2bffcfedede24e80399
ThgMelo <es.thg.melo@gmail.com>
2022-09-23 23:59:06 -0300

    Estilização e funcionalidades

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jogo Pong
+---
+Projeto desenvolvido com intuito de estudar lógica de programação.
+
+## Como jogar?
+- Para mover a raquete para cima: w
+- Para mover a raquete para baixo: s
--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,5 @@
 ctx = document.getElementById('tela').getContext('2d');
-let play = true;
+let play = false;
 
 //Variáveis Bolinha
 let raioBolinha = 10;
@@ -15,6 +15,7 @@ let comprimentoRaquete = 90;
 let posXRaqueteJogador = 5;
 let posYRaqueteJogador = 200-45;
 let velocidadeRaquete = 30;
+let pontuacaoJogador = 0;
 
 //Variáveis Oponente
 let pontuacaoOponente = 0;
@@ -22,9 +23,16 @@ let posXRaqueteOponente = 600-5 - larguraRaquete;
 let posYRaqueteOponente = 200-45;
 let velocidadeYOponente;
 
+//Elementos html
+let jogador = document.querySelector('#pontuacaoJogador');
+let oponente = document.querySelector('#pontuacaoOponente');
+let botaoPlay = document.querySelector('#play');
+let botaoPause = document.querySelector('#pause');
+let tela = document.querySelector('#tela');
+
 
 corFundo();
-//setInterval(game, 10);
+setInterval(game, 10);
 
 document.addEventListener('keypress', function(event){
     if(event.key == 'w' && posYRaqueteJogador > 20){
@@ -35,26 +43,66 @@ document.addEventListener('keypress', function(event){
     }
 });
 
+botaoPlay.addEventListener('click', ()=>{
+    tela.focus();
+    play = true;
 
+    
+});
+
+botaoPause.addEventListener('click', ()=>{
+    play = false;
+});
+
+
+let caminhoCorreto = true;
+let aleatorio = .5;
+setInterval(probabilidade, 5000);
+
+function probabilidade(){
+    aleatorio = Math.random();
+    if(aleatorio > 0.83){
+        caminhoCorreto = false;
+    }else{
+        caminhoCorreto = true;
+    }
+}
 
 //Funções
 function movimentaRaqueteOponente(){
-    posYRaqueteOponente = posYBolinha - comprimentoRaquete /2;
+    if(((posYBolinha - raioBolinha - comprimentoRaquete/2) >= 0) && ((posYBolinha) <= 395 - comprimentoRaquete/2)){
+        
+        if(caminhoCorreto){
+            posYRaqueteOponente = (posYBolinha - comprimentoRaquete/2);
+        }else{
+            posYBolinha<200? posYRaqueteOponente =  posYBolinha + 10: posYRaqueteOponente = posYBolinha - comprimentoRaquete -10;
+        }
+        
+    }
+    
 }
 
 function colisaoRaqueteJogador(){
     if(posXBolinha <= (larguraRaquete + posXRaqueteJogador + raioBolinha) && (posYBolinha >= posYRaqueteJogador) && (posYBolinha <= (posYRaqueteJogador+comprimentoRaquete))){
+        velocidadeBolinha *= -1;
+        velocidadeXBolinha *= -1;
+    }
+}
+
+function colisaoRaqueteOponente(){
+    if(posXBolinha == (590 - raioBolinha) && (posYBolinha >= posYRaqueteOponente) && (posYBolinha <= (posYRaqueteOponente+comprimentoRaquete))){
         velocidadeXBolinha *= -1;
     }
 }
 
 function pontosOponente(){
     if(posXBolinha - raioBolinha ==0){
-        
         pontuacaoOponente +=1;
-        console.log(pontuacaoOponente);
-
-    } 
+        oponente.innerHTML = pontuacaoOponente;
+    } else if(posXBolinha + raioBolinha == 600){
+        pontuacaoJogador +=1;
+        jogador.innerHTML = pontuacaoJogador;
+    }
 }
 
 function desenhaRaquete(posX, posY, largura, comprimento){
@@ -90,16 +138,20 @@ function colisaoParede(){
 }
 
 function game(){
-    limpaTela();
-    corFundo();
-    posXBolinha += velocidadeXBolinha;
-    posYBolinha += velocidadeYBolinha;
-    bolinha(raioBolinha, posXBolinha, posYBolinha);
-    colisaoParede();
+    if(play){
+        limpaTela();
+        corFundo();
+        posXBolinha += velocidadeXBolinha;
+        posYBolinha += velocidadeYBolinha;
+        bolinha(raioBolinha, posXBolinha, posYBolinha);
+        colisaoParede();
 
-    desenhaRaquete(posXRaqueteJogador, posYRaqueteJogador, larguraRaquete, comprimentoRaquete);
-    colisaoRaqueteJogador();
-    pontosOponente();
-    movimentaRaqueteOponente();
-    desenhaRaquete(posXRaqueteOponente, posYRaqueteOponente, larguraRaquete, comprimentoRaquete)
+        desenhaRaquete(posXRaqueteJogador, posYRaqueteJogador, larguraRaquete, comprimentoRaquete);
+        colisaoRaqueteJogador();
+        pontosOponente();
+        movimentaRaqueteOponente();
+        desenhaRaquete(posXRaqueteOponente, posYRaqueteOponente, larguraRaquete, comprimentoRaquete);
+        colisaoRaqueteOponente();
+    }
+    
 }
